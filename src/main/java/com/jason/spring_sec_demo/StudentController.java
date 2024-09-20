29c004f8fe92232fcc478431a42b0288fffd16c5
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-09-21 01:43:49 +0530

    Create getStudents method

--- src/main/java/com/jason/spring_sec_demo/StudentController.java
+++ src/main/java/com/jason/spring_sec_demo/StudentController.java
@@ -1,5 +1,22 @@
 package com.jason.spring_sec_demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class StudentController {
+
+    List<Student> students = new ArrayList<>(List.of(new Student(1,"Mohit","Java"),
+            new Student(2,"Kaukau","Blockchain")));
+
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        return students;
+    }
+
 }
