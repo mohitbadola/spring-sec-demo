d039dfd41b05dcf3a80acd940f4b69ac24b11889
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-09-28 19:04:55 +0530

    Create saveUser method

--- src/main/java/com/jason/spring_sec_demo/service/UserService.java
+++ src/main/java/com/jason/spring_sec_demo/service/UserService.java
@@ -1,7 +1,16 @@
 package com.jason.spring_sec_demo.service;
 
+import com.jason.spring_sec_demo.dao.UserRepo;
+import com.jason.spring_sec_demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepo repo;
+
+    public User saveUser(User user){
+        return repo.save(user);
+    }
 }
