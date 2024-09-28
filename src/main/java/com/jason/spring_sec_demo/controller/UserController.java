f1236e5233ab529d33aa6684349887994f5d5dbd
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-09-28 19:02:04 +0530

    Update UserController.java

--- src/main/java/com/jason/spring_sec_demo/controller/UserController.java
+++ src/main/java/com/jason/spring_sec_demo/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.jason.spring_sec_demo.controller;
 
+import com.jason.spring_sec_demo.model.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @PostMapping("register")
+    public User Register(@RequestBody User user){
+        return user;
+    }
 }
