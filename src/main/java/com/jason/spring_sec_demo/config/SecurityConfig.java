49159dc2ad7fcc0796f59f2c453e6b677c09f635
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-10-05 04:30:56 +0530

    Update SecurityConfig.java

--- src/main/java/com/jason/spring_sec_demo/config/SecurityConfig.java
+++ src/main/java/com/jason/spring_sec_demo/config/SecurityConfig.java
@@ -42,7 +42,6 @@ public class SecurityConfig {
                         .requestMatchers("register")
                         .permitAll()
                         .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
