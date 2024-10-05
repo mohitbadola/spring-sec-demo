39d0e3a6afbd1c977472d98bc8f75dcf4a33e6f0
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-10-05 18:56:02 +0530

    Update User.java

--- src/main/java/com/jason/spring_sec_demo/model/User.java
+++ src/main/java/com/jason/spring_sec_demo/model/User.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Table(name = "users")
 public class User {
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String password;
