63de3178f60a328d7f22b55ba8d6f36b3ad6fbdb
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-10-06 19:11:55 +0530

    Create generateToken method

--- src/main/java/com/jason/spring_sec_demo/service/JwtService.java
+++ src/main/java/com/jason/spring_sec_demo/service/JwtService.java
@@ -1,10 +1,23 @@
 package com.jason.spring_sec_demo.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
     public String generateToken(String username) {
 
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*3))
+                .signWith(getKey(), SignatureAlgorithm.HS256).compact();
     }
 }
