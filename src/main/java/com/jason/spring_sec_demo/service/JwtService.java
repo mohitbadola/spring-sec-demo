ea4ffc7b579832cc87cb14da4b3d193c5ab4720e
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-10-09 20:58:38 +0530

    Set AuthToken in SecurityContext

--- src/main/java/com/jason/spring_sec_demo/service/JwtService.java
+++ src/main/java/com/jason/spring_sec_demo/service/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
@@ -51,5 +52,13 @@ public class JwtService {
     private Key getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUserName(String token) {
+        return "";
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        return true;
     }
 }
