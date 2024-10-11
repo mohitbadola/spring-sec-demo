bf27c8c82f86d413795447ada3f097cf8c2a5eff
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-10-11 17:07:23 +0530

    Final commit

--- src/main/java/com/jason/spring_sec_demo/service/JwtService.java
+++ src/main/java/com/jason/spring_sec_demo/service/JwtService.java
@@ -1,9 +1,7 @@
 package com.jason.spring_sec_demo.service;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
